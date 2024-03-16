8308604f16c0beac40a4c4e6d0c36d8a8a1d8a4e
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-03-16 06:13:05 -0300

    Adicionando componente Button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { Button } from './Button'
+
+export default {
+    title: 'Button',
+    component: Button,
+}
+
+const onClick = () => {
+    console.log('Button clicked')
+}
+
+export const Solid = () => <Button type='solid' label='Solido' onClick={onClick} />
+
+export const Outline = () => <Button type='outline' label='Outline' onClick={onClick} />
+
+export const Text = () => <Button type='text' label='Text' onClick={onClick} />
