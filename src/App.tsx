191076f2b01a03d3e9347fe83e99c52e341aa438
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-03-27 14:01:47 -0300

    Adiciona autenticação do Google e comenta o formulário de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { routes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      {routes}
+      <GoogleOAuthProvider clientId='112629213943-ltci50f3petj9rfckhofatc6sve64btj.apps.googleusercontent.com'>
+        {routes}
+      </GoogleOAuthProvider>
     </BrowserRouter>
   )
 }
