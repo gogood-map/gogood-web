7cdfe5ce4bb0b4453087811209d15251d27c29c2
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-03-29 19:54:51 -0300

    Atualizações no formulário de autenticação e na página de autenticação

--- src/pages/AuthPage/components/AuthCard/AuthCard.tsx
+++ src/pages/AuthPage/components/AuthCard/AuthCard.tsx
@@ -1,27 +1,27 @@
 import { designTokens } from 'design-tokens'
 
 export type AuthCardProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export function AuthCard(props: AuthCardProps) {
 
-    return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: '350px',
-            height: 'auto',
-            maxHeight: '650px',
-            gap: designTokens.spacing.mediumLarge,
-            padding: designTokens.spacing.large,
-            borderRadius: designTokens.borderRadius.medium,
-            boxShadow: `0px 4px 13.9px 0px ${designTokens.color.boxShadow}`,
-            backgroundColor: designTokens.color.white,
-        }}>
-            {props.children}
-        </div>
-    )
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      width: '350px',
+      height: 'auto',
+      maxHeight: '650px',
+      gap: designTokens.spacing.mediumLarge,
+      padding: designTokens.spacing.large,
+      borderRadius: designTokens.borderRadius.medium,
+      boxShadow: `0px 4px 13.9px 0px ${designTokens.color.boxShadow}`,
+      backgroundColor: designTokens.color.white,
+    }}>
+      {props.children}
+    </div>
+  )
 }
