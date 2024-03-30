eca393407171d88b2862d99540d216e37d48fb32
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-03-29 23:48:04 -0300

    Adiciona pacote @vercel/speed-insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { routes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <GoogleOAuthProvider clientId={clientId ? clientId : ''}>
         {routes}
+        <SpeedInsights/>
       </GoogleOAuthProvider>
     </BrowserRouter>
   )
