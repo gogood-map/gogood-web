39e8e5301c7a5d2324a7c19db6776f00d770bcca
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-03-30 17:24:15 -0300

    Removido pacote @vercel/speed-insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { routes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <GoogleOAuthProvider clientId={clientId ? clientId : ''}>
         {routes}
-        <SpeedInsights/>
       </GoogleOAuthProvider>
     </BrowserRouter>
   )
