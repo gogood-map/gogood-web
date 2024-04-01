ebde7930ab12cd7e9ad008992734f4fd985dd549
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-04-01 01:03:33 -0300

    Adicionado provedor de autenticação e .env ao .gitignore

---
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react';
+
+type User = {
+    name: string
+    email: string
+    picture: string
+}
+
+type AuthContextType = {
+    user: User | null;
+    login: (userData: User) => void;
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
+
+export function AuthProvider(props: { children: React.ReactNode }) {
+    const [user, setUser] = useState<User | null>(null);
+
+    const login = (userData: User) => {
+        setUser(userData);
+    };
+
+    const logout = () => {
+        setUser(null);
+    };
+
+    const authContextValue: AuthContextType = {
+        user,
+        login,
+        logout,
+    };
+
+    return <AuthContext.Provider value={authContextValue}>{props.children}</AuthContext.Provider>;
+};
