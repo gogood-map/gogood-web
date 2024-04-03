e2a93e1b1d4f78a06735fc61d0af82968ee0da5d
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-04-03 14:59:37 -0300

    Atualizações no AuthProvider.tsx

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 
-type User = {
+export type User = {
     name: string
     email: string
     picture: string
@@ -17,7 +17,11 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
     if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
+        return {
+            user: null,
+            login: () => { },
+            logout: () => { },
+        };
     }
     return context;
 };
