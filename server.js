727959e89d86773a819305325c59573faa63c479
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-04-04 12:01:21 -0300

    Removido pacote Express e adicionado servidor Node.js com Express e CORS

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const server = express()
+server.use(cors())
+
+server.use(express.static('dist'))
+
+server.get('/', (req, res) => {
+    res.sendFile('dist/index.html')
+})
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000/')
+})
