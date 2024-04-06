0aecdc8ae0e95704eea521730f617af8c47e1fca
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-04-05 23:32:44 -0300

    Adiciona componentes Sidebar e SidebarItem

---
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,23 @@
+import { RiHistoryFill } from "react-icons/ri";
+import { SidebarItem } from "../SidebarItem/SidebarItem";
+import { Sidebar } from "./Sidebar";
+import { FiHome, FiMap, FiSettings, FiUser } from "react-icons/fi";
+
+export default {
+    title: "Components/Sidebar",
+    component: Sidebar,
+};
+
+const defaultIconSize = '28px';
+
+export const Default = () => {
+    return (
+        <Sidebar>
+            <SidebarItem icon={<FiHome size={defaultIconSize} />} text="Início" active={true} alert />
+            <SidebarItem icon={<FiMap size={defaultIconSize} />} text="Mapa" active={false} />
+            <SidebarItem icon={<FiUser size={defaultIconSize} />} text="Perfil" active={false} />
+            <SidebarItem icon={<RiHistoryFill size={defaultIconSize} />} text="Histórico" active={false} />
+            <SidebarItem icon={<FiSettings size={defaultIconSize} />} text="Ajustes" active={false} />
+        </Sidebar>
+    );
+};
---
+++ src/components/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,92 @@
+import { ReactNode, useContext, useState } from "react";
+import { SidebarContext, SidebarContextProps } from "../Sidebar/Sidebar";
+import { designTokens } from "design-tokens";
+
+export type SidebarItemProps = {
+    icon: ReactNode;
+    text: string;
+    active?: boolean;
+    alert?: boolean;
+}
+
+export function SidebarItem({ icon, text, alert }: SidebarItemProps) {
+    const { expanded } = useContext(SidebarContext) as SidebarContextProps
+    const [isHovered, setIsHovered] = useState(false)
+
+    const toolTipStyle = {
+        position: 'absolute',
+        left: '100%',
+        borderRadius: '0.375rem',
+        padding: '0.25rem 0.5rem',
+        marginLeft: designTokens.spacing.small,
+        backgroundColor: designTokens.color.background,
+        color: designTokens.color.text,
+        fontSize: designTokens.font.size.medium,
+        visibility: 'hidden',
+        opacity: '0',
+        transform: `translateX(-${designTokens.spacing.tiny})`,
+        transition: 'visibility 0s, opacity 0.3s, transform 0.3s',
+    } as React.CSSProperties
+
+    const hoveredToolTipStyle = {
+        visibility: 'visible',
+        opacity: '1',
+        transform: 'translateX(0)'
+    } as React.CSSProperties
+
+
+    return (
+        <li
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                fontWeight: designTokens.font.weight.medium,
+                borderRadius: designTokens.borderRadius.medium,
+                color: designTokens.color.text,
+                width:'fit-content',
+                padding: 0,
+                cursor: 'pointer',
+            }}
+            onClick={() => { console.log('clicked') }}
+        >
+            {icon}
+            <span
+                style={{
+                    overflow: 'hidden',
+                    transition: 'width 0.3s ease, margin-left 0.3s ease',
+                    width: expanded ? '100px' : '0',
+                    marginLeft: expanded ? designTokens.spacing.medium : '0',
+                    fontSize: designTokens.font.size.mediumLarge,
+                }}
+            >
+                {text}
+            </span>
+            {alert && (
+                <div
+                    style={{
+                        position: expanded ? 'unset' : 'absolute',
+                        margin: expanded ? '0' : '0 0 22px 22px',
+                        backgroundColor: designTokens.color.primary,
+                        color: designTokens.color.white,
+                        borderRadius: '50%',
+                        width: '0.5rem',
+                        height: '0.5rem',
+                        transition: 'margin 1s ease',
+                    }}
+                />
+            )}
+
+            {!expanded && (
+                <div
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    style={isHovered ? { ...toolTipStyle, ...hoveredToolTipStyle } : toolTipStyle}
+                >
+                    {text}
+                </div>
+            )}
+        </li>
+    )
+}
