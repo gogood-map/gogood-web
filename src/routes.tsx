7ac7b22a843875636bea0b5d5d36f61766eac69c
MatheusHenrique055 <matheus.rsantos@sptech.school>
2024-04-05 23:33:43 -0300

    Adiciona rota para SidebarLayout no arquivo routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,15 @@ import { Layout } from './pages/Layout/Layout.tsx'
 import { AuthPage } from './pages/AuthPage/AuthPage.tsx'
 import { Home } from './pages/Home/Home.tsx'
 import { NotFound } from './pages/NotFound/NotFound.tsx'
+import { SidebarLayout } from './pages/SidebarLayout/SidebarLayout.tsx'
 
 export const routes = (
     <Routes>
         <Route path='' element={<Layout />}>
             <Route path='' element={<Home />} />
+        </Route>
+        <Route path='sidebartexte' element={<SidebarLayout />}>
+            
         </Route>
         <Route path='cadastro' element={<AuthPage type='register' />} />
         <Route path='login' element={<AuthPage type='login' />} />
