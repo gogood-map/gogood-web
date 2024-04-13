3d0d9a4b4200c0c8a438bd6ca6feff4cef219ee1
TheusBr05 <mat.587263@gmail.com>
2024-04-13 17:40:42 -0300

    Adicionar página de Mapa e componentes relacionados

---
+++ src/pages/Map/Map.tsx
@@ -0,0 +1,11 @@
+import { MapComponent } from './components/MapComponent/MapComponent'
+import { RouteSearchCard } from './components/RouteSearchCard/RouteSearchCard'
+
+export function Map() {
+    return (
+        <>
+            <MapComponent />
+            <RouteSearchCard />
+        </>
+    )
+}
