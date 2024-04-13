3c78c03979c2a3ee651b6d6a5dc39d4e903f2f9e
TheusBr05 <mat.587263@gmail.com>
2024-04-13 17:40:42 -0300

    Adicionar página de Mapa e componentes relacionados

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,39 +1,11 @@
-import { useState } from 'react'
 import { MapComponent } from './components/MapComponent/MapComponent'
 import { RouteSearchCard } from './components/RouteSearchCard/RouteSearchCard'
-import { RoutesResponse } from './components/RoutesSelection/RoutesSelection'
 
 export function Map() {
-    const [routes, setRoutes] = useState<RoutesResponse[]>()
-    const [searchStatus, setSearchStatus] = useState<'loading' | 'success' | 'error' | 'none'>('none')
-
-    const handleSubmitSearch = (origin: string, destination: string, travelMode: string) => {
-        setSearchStatus('loading')
-        consultaRota(origin, destination, travelMode)
-            .then((rotas) => {
-                setRoutes(rotas)
-                console.log(rotas)
-                setSearchStatus('success')
-            })
-            .catch((error) => {
-                console.error(error)
-                setSearchStatus('error')
-            })
-    }
-
-    const consultaRota = async (origin: string, destination: string, travelMode: string) => {
-        const response = await fetch(`https://gogood.brazilsouth.cloudapp.azure.com/rotas/${travelMode}?origem=${origin}&destino=${destination}`)
-        if (!response.ok) {
-            return Promise.reject('Erro ao consultar rotas')
-        }   
-        const json = await response.json()
-
-        return await json as RoutesResponse[]
-    }
     return (
         <>
-            <MapComponent routes={routes} />
-            <RouteSearchCard onSubmitSearch={handleSubmitSearch} routes={routes} searchStatus={searchStatus} />
+            <MapComponent />
+            <RouteSearchCard />
         </>
     )
 }
--- src/pages/Map/components/MapComponent/MapComponent.tsx
+++ src/pages/Map/components/MapComponent/MapComponent.tsx
@@ -1,41 +1,33 @@
-import { useEffect, useState, useCallback } from 'react'
-import { Loader } from '@googlemaps/js-api-loader'
-import { RoutesResponse } from '../RoutesSelection/RoutesSelection'
-import { designTokens } from 'design-tokens'
+import { useEffect, useState, useCallback } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 
-export type MapComponentProps = {
-    routes?: RoutesResponse[]
-}
-
-export function MapComponent(props: MapComponentProps) {
-    const { routes } = props
-    const [map, setMap] = useState<google.maps.Map | null>(null)
-    const [heatmap, setHeatmap] = useState<google.maps.visualization.HeatmapLayer | null>(null)
-    const [polyline, setPolyline] = useState<google.maps.Polyline[] | null>(null)
-    const [data, setData] = useState<google.maps.LatLng[]>([])
+export function MapComponent() {
+    const [map, setMap] = useState<google.maps.Map | null>(null);
+    const [heatmap, setHeatmap] = useState<google.maps.visualization.HeatmapLayer | null>(null);
+    const [data, setData] = useState<google.maps.LatLng[]>([]);
     const loader = new Loader({
         apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
         version: 'weekly',
         libraries: ['visualization'],
-    })
+    });
 
     // Função de debounce
     const debounce = (func: (...args: any[]) => void, wait: number) => {
-        let timeout: NodeJS.Timeout
+        let timeout: NodeJS.Timeout;
         return function executedFunction(...args: any[]) {
             const later = () => {
-                clearTimeout(timeout)
-                func(...args)
-            }
-            clearTimeout(timeout)
-            timeout = setTimeout(later, wait)
-        }
-    }
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    };
 
 
     const debouncedLoadData = useCallback(debounce((lat, lng) => {
-        loadData(lat, lng).then(newData => setData(newData))
-    }, 500), [])
+        loadData(lat, lng).then(newData => setData(newData));
+    }, 500), []);
 
     useEffect(() => {
         loader.load().then(() => {
@@ -45,81 +37,48 @@ export function MapComponent(props: MapComponentProps) {
                 mapTypeControl: false,
                 streetViewControl: false,
                 fullscreenControl: false,
-            })
+            });
 
             map.addListener('center_changed', () => {
-                const center = map.getCenter()
+                const center = map.getCenter();
                 if (center) {
-                    debouncedLoadData(center.lat(), center.lng())
+                    debouncedLoadData(center.lat(), center.lng());
                 }
-            })
+            });
 
-            setMap(map)
-            loadData(-23.5581213, -46.661614).then(newData => setData(newData))
-        })
-    }, [])
-
+            setMap(map);
+        });
+    }, []);
 
     useEffect(() => {
         if (map && data.length) {
             if (heatmap) {
-                heatmap.setMap(null)
+                heatmap.setMap(null);
             }
             const newHeatmap = new google.maps.visualization.HeatmapLayer({
                 data,
                 map,
                 maxIntensity: 7,
-            })
-            setHeatmap(newHeatmap)
+            });
+            setHeatmap(newHeatmap);
         }
-    }, [data, map])
-
-    useEffect(() => {
-        if (polyline && polyline?.length > 0) {
-            polyline.forEach(polyline => {
-                polyline.setMap(null)
-            })
-        }
-        if (routes) {
-            const listaPolyline: google.maps.Polyline[] = []
-
-            routes.forEach(async (route, index) => {
-                const { encoding } = await google.maps.importLibrary("geometry") as google.maps.GeometryLibrary
-                const caminho = encoding.decodePath(route.polyline)
-                const polylineRota = new google.maps.Polyline({
-                    path: caminho,
-                    geodesic: true,
-                    strokeColor: index === 0 ? designTokens.color.success : index === 1 ? designTokens.color.alert : designTokens.color.error,
-                    strokeOpacity: 1.0,
-                    strokeWeight: 5,
-                })
-                listaPolyline.push(polylineRota)
-                polylineRota.setMap(map)
-            })
-
-            setPolyline(listaPolyline)
-
-        }
-
-    }, [routes])
+    }, [data, map]);
 
     const loadData = async (lat: number, lng: number) => {
         try {
-            const response = await fetch(`https://gogood.brazilsouth.cloudapp.azure.com/consultar/local/${lat}/${lng}`, {
+            const response = await fetch(`http://52.226.122.160:8080/consultar/local/${lat}/${lng}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            })
-            const json = await response.json()
-            return json.mapData.map((item: { latitude: number; longitude: number }) => {
-                new google.maps.LatLng(item.latitude, item.longitude)
-            })
+            });
+            const json = await response.json();
+            return json.mapData.map((item: { latitude: number; longitude: number; }) => new google.maps.LatLng(item.latitude, item.longitude));
         } catch (error) {
-            console.error(error)
-            return []
+            console.error(error);
+            return [];
         }
-    }
+    };
 
-    return <div id="map" style={{ width: '100%', height: '100%' }} />
+    return <div id="map" style={{ width: '100%', height: '100%' }}></div>;
 }
