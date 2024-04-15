dec1062c388988cedf74629ac47fcbc24a808d54
TheusBr05 <mat.587263@gmail.com>
2024-04-15 13:37:13 -0300

    Atualizar componente MapComponent para exibir rotas no mapa

--- src/pages/Map/components/MapComponent/MapComponent.tsx
+++ src/pages/Map/components/MapComponent/MapComponent.tsx
@@ -1,34 +1,39 @@
-import { useEffect, useState, useCallback } from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+import { useEffect, useState, useCallback } from 'react'
+import { Loader } from '@googlemaps/js-api-loader'
+import { RoutesResponse } from '../RoutesSelection/RoutesSelection'
 
+export type MapComponentProps = {
+    routes?: RoutesResponse[]
+}
 
-export function MapComponent() {
-    const [map, setMap] = useState<google.maps.Map | null>(null);
-    const [heatmap, setHeatmap] = useState<google.maps.visualization.HeatmapLayer | null>(null);
-    const [data, setData] = useState<google.maps.LatLng[]>([]);
+export function MapComponent(props: MapComponentProps) {
+    const { routes } = props
+    const [map, setMap] = useState<google.maps.Map | null>(null)
+    const [heatmap, setHeatmap] = useState<google.maps.visualization.HeatmapLayer | null>(null)
+    const [data, setData] = useState<google.maps.LatLng[]>([])
     const loader = new Loader({
         apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
         version: 'weekly',
         libraries: ['visualization'],
-    });
+    })
 
     // Função de debounce
     const debounce = (func: (...args: any[]) => void, wait: number) => {
-        let timeout: NodeJS.Timeout;
+        let timeout: NodeJS.Timeout
         return function executedFunction(...args: any[]) {
             const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    };
+                clearTimeout(timeout)
+                func(...args)
+            }
+            clearTimeout(timeout)
+            timeout = setTimeout(later, wait)
+        }
+    }
 
 
     const debouncedLoadData = useCallback(debounce((lat, lng) => {
-        loadData(lat, lng).then(newData => setData(newData));
-    }, 500), []);
+        loadData(lat, lng).then(newData => setData(newData))
+    }, 500), [])
 
     useEffect(() => {
         loader.load().then(() => {
@@ -38,62 +43,57 @@ export function MapComponent() {
                 mapTypeControl: false,
                 streetViewControl: false,
                 fullscreenControl: false,
-            });
-
-            consultaRota().then(async(rota:any)=>{
-                const {encoding} = await google.maps.importLibrary("geometry") as google.maps.GeometryLibrary
-                const caminho = encoding.decodePath(rota.polyline)
-                const polyline = new google.maps.Polyline({
-                    path: caminho,
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    map: map 
-                })
-                
-                polyline.setMap(map)
-                
-                console.log(caminho);
-                
             })
-                       
+
             map.addListener('center_changed', () => {
-                const center = map.getCenter();
+                const center = map.getCenter()
                 if (center) {
-                    debouncedLoadData(center.lat(), center.lng());
+                    debouncedLoadData(center.lat(), center.lng())
                 }
-            });
+            })
 
-            setMap(map);
+            setMap(map)
 
-            const center = map.getCenter();
-            if (center) {
-                loadData(center.lat(), center.lng()).then(newData => setData(newData));
-            }
-            
-        });
-    }, []);
+        })
+    }, [])
 
-    const consultaRota = async () =>{
-        const response = await fetch('https://gogood.brazilsouth.cloudapp.azure.com/rotas/bike?origem=Faculdade%20SPTECH&destino=Parque%20Ibirapuera')
-        const json = await response.json();
 
-        return await json[1]
-    }
-    
     useEffect(() => {
         if (map && data.length) {
             if (heatmap) {
-                heatmap.setMap(null);
+                heatmap.setMap(null)
             }
             const newHeatmap = new google.maps.visualization.HeatmapLayer({
                 data,
                 map,
                 maxIntensity: 7,
-            });
-            setHeatmap(newHeatmap);
+            })
+            setHeatmap(newHeatmap)
         }
-    }, [data, map]);
+    }, [data, map])
+
+    useEffect(() => {
+        if (map) {
+            map.data.forEach((feature) => {
+                map.data.remove(feature)
+            })
+        }
+        if (routes) {
+            routes.forEach(async (route) => {
+                const { encoding } = await google.maps.importLibrary("geometry") as google.maps.GeometryLibrary
+                const caminho = encoding.decodePath(route.polyline)
+                const polyline = new google.maps.Polyline({
+                    path: caminho,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    map: map
+                })
+                polyline.setMap(map)
+            })
+        }
+           
+    }, [routes])
 
     const loadData = async (lat: number, lng: number) => {
         try {
@@ -102,17 +102,17 @@ export function MapComponent() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            });
-            const json = await response.json();
-            return json.mapData.map((item: { latitude: number; longitude: number; }) => new google.maps.LatLng(item.latitude, item.longitude));
+            })
+            const json = await response.json()
+            return json.mapData.map((item: { latitude: number; longitude: number }) => new google.maps.LatLng(item.latitude, item.longitude))
         } catch (error) {
-            console.error(error);
-            return [];
+            console.error(error)
+            return []
         }
-    };
+    }
 
 
-    
 
-    return <div id="map" style={{ width: '100%', height: '100%' }}></div>;
+
+    return <div id="map" style={{ width: '100%', height: '100%' }}></div>
 }
