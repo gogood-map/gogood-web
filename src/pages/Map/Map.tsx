7752b7bd5e28210e43be6d23c108091e7ba6d858
TheusBr05 <mat.587263@gmail.com>
2024-04-15 13:37:27 -0300

    Atualizar componente MapComponent para exibir rotas no mapa

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import { MapComponent } from './components/MapComponent/MapComponent'
 import { RouteSearchCard } from './components/RouteSearchCard/RouteSearchCard'
+import { RoutesResponse } from './components/RoutesSelection/RoutesSelection'
 
 export function Map() {
+    const [routes, setRoutes] = useState<RoutesResponse[]>()
+
+    const handleSubmitSearch = (origin: string, destination: string, travelMode: string) => {
+        consultaRota(origin, destination, travelMode)
+            .then((rotas) => {
+                setRoutes(rotas)
+                console.log(rotas)
+            })
+    }
+
+    const consultaRota = async (origin: string, destination: string, travelMode: string) => {
+        const response = await fetch(`https://gogood.brazilsouth.cloudapp.azure.com/rotas/${travelMode}?origem=${origin}&destino=${destination}`)
+        const json = await response.json()
+
+        return await json as RoutesResponse[]
+    }
     return (
         <>
-            <MapComponent />
-            <RouteSearchCard />
+            <MapComponent routes={routes} />
+            <RouteSearchCard onSubmitSearch={handleSubmitSearch} routes={routes} />
         </>
     )
 }
--- src/pages/Map/components/RouteSearchCard/RouteSearchCard.tsx
+++ src/pages/Map/components/RouteSearchCard/RouteSearchCard.tsx
@@ -10,25 +10,15 @@ export type RouteSearchCardContextProps = {
 
 export const RouteSearchCardContext = createContext<RouteSearchCardContextProps | undefined>(undefined)
 
-export function RouteSearchCard() {
+export type RouteSearchCardProps = {
+    routes?: RoutesResponse[]
+    onSubmitSearch: (origin: string, destination: string, travelMode: string) => void
+}
+
+export function RouteSearchCard(props: RouteSearchCardProps) {
+    const { routes, onSubmitSearch } = props
     const [expandedCard, setExpandedCard] = useState(true)
     const { expanded } = useContext(SidebarContext) as SidebarContextProps
-    const [routes, setRoutes] = useState<RoutesResponse[]>()
-
-    const handleSubmit = (origin: string, destination: string, travelMode: string) => {
-        consultaRota(origin, destination, travelMode)
-            .then((rotas) => {
-                setRoutes(rotas)
-                console.log(rotas)
-            })
-    }
-
-    const consultaRota = async (origin: string, destination: string, travelMode: string) => {
-        const response = await fetch(`https://gogood.brazilsouth.cloudapp.azure.com/rotas/${travelMode}?origem=${origin}&destino=${destination}`)
-        const json = await response.json();
-
-        return await json as RoutesResponse[]
-    }
 
     return (
         <RouteSearchCardContext.Provider value={{ expandedCard }}>
@@ -45,7 +35,7 @@ export function RouteSearchCard() {
                 backgroundColor: 'transparent',
                 transition: 'left 0.3s ease',
             }}>
-                <RouteForm onClickExpand={() => { setExpandedCard(!expandedCard) }} onSubmit={handleSubmit} />
+                <RouteForm onClickExpand={() => { setExpandedCard(!expandedCard) }} onSubmit={onSubmitSearch} />
                 {routes && <RoutesSelection routes={routes} />}
             </div>
         </RouteSearchCardContext.Provider>
