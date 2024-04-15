89c8e2d4ae05cf8e7daa2dcec69140027d15a6de
TheusBr05 <mat.587263@gmail.com>
2024-04-15 02:28:01 -0300

    Atualizar exibição das rotas disponíveis para escolha

--- src/pages/Map/components/RoutesSelection/RoutesSelection.tsx
+++ src/pages/Map/components/RoutesSelection/RoutesSelection.tsx
@@ -3,8 +3,59 @@ import { useContext } from "react";
 import { RouteSearchCardContext, RouteSearchCardContextProps } from "../RouteSearchCard/RouteSearchCard";
 import { RouteOption } from "../RouteOption/RouteOption";
 
-export function RoutesSelection() {
+export type RoutesResponse = {
+    origem: string
+    destino: string
+    distancia: number
+    duracao: string
+    horarioSaida: string
+    horarioChegada: string
+    qtdOcorrenciasTotais: number
+    polyline: string
+    etapas: {
+        instrucao: string
+    }[]
+}
+
+type RoutesSelectionProps = {
+    routes: RoutesResponse[]
+}
+
+export function RoutesSelection(props: RoutesSelectionProps) {
+    const { routes } = props
     const { expandedCard } = useContext(RouteSearchCardContext) as RouteSearchCardContextProps
+
+    const orderedRoutes = (routes: RoutesResponse[]) => routes.sort((a, b) => {
+        const durationA = stringToMinutes(a.duracao);
+        const durationB = stringToMinutes(b.duracao);
+
+        return durationA - durationB;
+    });
+
+    const stringToMinutes = (duration: string) => {
+        const parts = duration.split(' ');
+        let totalMinutos = 0;
+
+        for (let i = 0; i < parts.length; i += 2) {
+            const value = parseInt(parts[i], 10);
+            const unit = parts[i + 1];
+
+            switch (unit) {
+                case 'hora':
+                case 'horas':
+                    totalMinutos += value * 60;
+                    break;
+                case 'minuto':
+                case 'minutos':
+                    totalMinutos += value;
+                    break;
+                default:
+                    throw new Error(`Unidade inválida: ${unit}`);
+            }
+        }
+
+        return totalMinutos;
+    }
 
     return (
         <div style={{
@@ -13,7 +64,7 @@ export function RoutesSelection() {
             justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
-            height: expandedCard ? '180px' : '0px',
+            height: expandedCard ? `calc(57px + (35 * ${routes.length}))` : '0px',
             overflow: 'hidden',
             gap: designTokens.spacing.medium,
             padding: expandedCard
@@ -50,10 +101,12 @@ export function RoutesSelection() {
                 width: '70%'
             }}>
 
-                <RouteOption risk="Risco Baixo" durationInMinutes={30} color={designTokens.color.success} />
-                <RouteOption risk="Risco Médio" durationInMinutes={60} color={designTokens.color.alert} />
-                <RouteOption risk="Risco Alto" durationInMinutes={130} color={designTokens.color.error} />
-
+                {orderedRoutes(routes).map((route, index) => {
+                    const durationInMinutes = stringToMinutes(route.duracao);
+                    const color = index === 0 ? designTokens.color.success : index === 1 ? designTokens.color.alert : designTokens.color.error;
+                    const risk = index === 0 ? 'Menor Risco' : index === 1 ? 'Risco Médio' : 'Risco Alto';
+                    return <RouteOption risk={risk} durationInMinutes={durationInMinutes} color={color} />;
+                })}
             </div>
         </div>
     )
