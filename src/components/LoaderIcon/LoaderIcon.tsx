524ee8527b117edd0b252ea4a7609040153a62a4
TheusBr05 <mat.587263@gmail.com>
2024-04-16 14:16:40 -0300

    Adicionar componente LoaderIcon

---
+++ src/components/LoaderIcon/LoaderIcon.tsx
@@ -0,0 +1,23 @@
+import { TbLoader2 } from "react-icons/tb";
+import style from './LoaderIcon.module.css'
+
+type LoaderIconProps = {
+    size?: number
+}
+
+export function LoaderIcon(props: LoaderIconProps) {
+    const { size } = props
+
+    return (
+        <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <TbLoader2 className={style.loader_icon} size={size} />
+
+        </div>
+    )
+}
