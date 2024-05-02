733b68b763f24188fd013b50ee32bc88e9c52204
TheusBr05 <mat.587263@gmail.com>
2024-05-02 00:02:36 -0300

    Adicionar componente de Histórico

---
+++ src/pages/History/History.tsx
@@ -0,0 +1,13 @@
+export function History() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: '',
+      alignItems: 'center',
+      height: '100%',
+    
+    }}>
+      <h1>History</h1>
+    </div>
+  )
+}
