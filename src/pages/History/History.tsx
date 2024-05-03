426c3047308c5419dc35cff24ec08b04e62bd22a
TheusBr05 <mat.587263@gmail.com>
2024-05-03 04:34:57 -0300

    Adicionar componente de Histórico

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,13 +1,104 @@
+import { designTokens } from 'design-tokens'
+import { MapComponent } from '../../components/MapComponent/MapComponent'
+import { HistoryTable } from './components/HistoryTable/HistoryTable'
+import { HistoryNav } from './components/HitoryNav/HistoryNav'
+
 export function History() {
+  const items = [
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+    {
+      date: 'Quinta-feira, 25 Jan, 2024',
+      origin: 'Rua A, 123',
+      destination: 'Rua B, 456',
+      onClick: () => console.log('clicked'),
+    },
+  ]
+
   return (
     <div style={{
       display: 'flex',
-      justifyContent: '',
-      alignItems: 'center',
-      height: '100%',
-    
+      flexDirection: 'column',
+      minHeight: '100%',
+      width: '100%',
+      position: 'relative',
     }}>
-      <h1>History</h1>
+      <HistoryNav />
+      <div style={{
+        display: 'flex',
+        marginLeft: '60px',
+        flex: 1,
+        position: 'relative',
+      }}>
+        <div style={{
+          width: '35%',
+          height: `calc(100vh - 41px - (${designTokens.spacing.medium}) * 2)`,
+        }}>
+          <HistoryTable items={items} />
+        </div>
+        <div style={{
+          margin: '20px',
+          width: '65%',
+          height: 'calc(100% - 40px)',
+          borderRadius: designTokens.borderRadius.medium,
+          boxShadow: ` 0px 4px 4px 2px ${designTokens.color.boxShadow}`,
+          overflow: 'hidden',
+        }}>
+          <MapComponent />
+        </div>
+      </div>
     </div>
   )
 }
