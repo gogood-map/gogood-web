df2c54c717a29df164af5bb3ca97ee69603c81e0
TheusBr05 <mat.587263@gmail.com>
2024-05-06 15:05:30 -0300

    Atualizar componente AddressList para exibir cidade e CEP dos endereços

--- src/pages/Profile/components/AddressForm/AddressForm.tsx
+++ src/pages/Profile/components/AddressForm/AddressForm.tsx
@@ -1,7 +1,9 @@
 import { designTokens } from 'design-tokens'
 import { useForm } from 'react-hook-form'
 import { BiHomeAlt2, BiHomeHeart } from 'react-icons/bi'
+import { IoSchoolOutline } from 'react-icons/io5'
 import { PiSuitcaseSimple } from 'react-icons/pi'
+import { Button } from '../../../../components/Button/Button'
 
 export function AddressForm() {
   const { register, watch } = useForm()
@@ -15,6 +17,7 @@ export function AddressForm() {
     padding: designTokens.spacing.small,
     borderRadius: designTokens.borderRadius.medium,
     border: `1px solid ${designTokens.color.border}`,
+    fontSize: designTokens.font.size.smallMedium,
     cursor: 'pointer'
   } as React.CSSProperties
 
@@ -28,23 +31,24 @@ export function AddressForm() {
     <div style={{
       display: 'flex',
       flexDirection: 'column',
-      width: `calc(100% - ${designTokens.spacing.medium} * 2)`,
+      width: `calc(100% - ${designTokens.spacing.mediumLarge} * 2)`,
       height: `calc(100% - ${designTokens.spacing.medium} * 2)`,
-      padding: designTokens.spacing.medium,
+      padding: `${designTokens.spacing.medium} ${designTokens.spacing.mediumLarge}`,
       backgroundColor: 'white',
       borderRadius: designTokens.borderRadius.large,
       boxShadow: `0px 4px 13.9px 0px ${designTokens.color.boxShadow}`,
+      gap: designTokens.spacing.medium,
     }}>
       <h1 style={{
         fontSize: designTokens.font.size.large,
         fontWeight: designTokens.font.weight.bold,
         margin: 0,
-        marginLeft: designTokens.spacing.small
       }}>Adicionar endereços</h1>
       <form style={{
         display: 'flex',
         flexDirection: 'column',
-        gap: designTokens.spacing.medium,
+        gap: designTokens.spacing.tiny,
+        justifyContent: 'space-between',
         flex: 1,
       }}>
         <div style={{
@@ -52,27 +56,137 @@ export function AddressForm() {
           flexDirection: 'column',
           gap: designTokens.spacing.tiny
         }}>
-          <label htmlFor='address'>Endereço</label>
-          <input
-            type='text'
-            id='address'
-            {...register('address', { required: true })}
-            style={{
-              padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
-              borderRadius: designTokens.borderRadius.medium,
-              border: `1px solid ${designTokens.color.border}`,
-              outline: 'none',
-              fontSize: designTokens.font.size.medium,
-              height: '25px',
-            }}
-          />
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}>
+            <label htmlFor='address' style={{
+              fontSize: designTokens.font.size.smallMedium
+            }}>CEP</label>
+            <input
+              type='text'
+              id='address'
+              {...register('address', { required: true })}
+              style={{
+                padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
+                borderRadius: designTokens.borderRadius.medium,
+                border: `1px solid ${designTokens.color.border}`,
+                outline: 'none',
+                fontSize: designTokens.font.size.medium,
+                height: '22px',
+                width: `calc(30% - ${designTokens.spacing.mediumLarge} * 2)`,
+              }}
+            />
+          </div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}>
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column',
+              marginRight: designTokens.spacing.medium,
+              width: `calc(70% - ${designTokens.spacing.medium})`,
+            }}>
+              <label htmlFor='street' style={{
+                fontSize: designTokens.font.size.smallMedium
+              }}>Rua</label>
+              <input
+                type='text'
+                id='street'
+                {...register('street', { required: true })}
+                style={{
+                  padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
+                  borderRadius: designTokens.borderRadius.medium,
+                  border: `1px solid ${designTokens.color.border}`,
+                  outline: 'none',
+                  fontSize: designTokens.font.size.medium,
+                  height: '22px',
+                }}
+              />
+            </div>
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '30%',
+            }}>
+              <label htmlFor='number' style={{
+                fontSize: designTokens.font.size.smallMedium
+              }}>Número</label>
+              <input
+                type='text'
+                id='number'
+                {...register('number', { required: true })}
+                style={{
+                  padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
+                  borderRadius: designTokens.borderRadius.medium,
+                  border: `1px solid ${designTokens.color.border}`,
+                  outline: 'none',
+                  fontSize: designTokens.font.size.medium,
+                  height: '22px',
+                }}
+              />
+            </div>
+          </div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}>
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column',
+              marginRight: designTokens.spacing.medium,
+              width: `calc(50% - ${designTokens.spacing.medium})`,
+            }}>
+              <label htmlFor='district' style={{
+                fontSize: designTokens.font.size.smallMedium
+              }}>Bairro</label>
+              <input
+                type='text'
+                id='district'
+                {...register('district', { required: true })}
+                style={{
+                  padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
+                  borderRadius: designTokens.borderRadius.medium,
+                  border: `1px solid ${designTokens.color.border}`,
+                  outline: 'none',
+                  fontSize: designTokens.font.size.medium,
+                  height: '22px',
+                }}
+              />
+            </div>
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '50%',
+            }}>
+              <label htmlFor='city' style={{
+                fontSize: designTokens.font.size.smallMedium
+              }}>Cidade</label>
+              <input
+                type='text'
+                id='city'
+                {...register('city', { required: true })}
+                style={{
+                  padding: `${designTokens.spacing.small} ${designTokens.spacing.medium}`,
+                  borderRadius: designTokens.borderRadius.medium,
+                  border: `1px solid ${designTokens.color.border}`,
+                  outline: 'none',
+                  fontSize: designTokens.font.size.medium,
+                  height: '22px',
+                }}
+              />
+            </div>
+          </div>
         </div>
         <div style={{
           display: 'flex',
           flexDirection: 'column',
           gap: designTokens.spacing.tiny
         }}>
-          <label>Etiqueta (opcional)</label>
+          <label style={{
+            fontSize: designTokens.font.size.smallMedium
+          }} >Etiqueta (opcional)</label>
           <div style={{
             display: 'flex',
             flexDirection: 'row',
@@ -87,6 +201,13 @@ export function AddressForm() {
               {...register('tag', { required: true })}
             />
             <input
+              id='partner'
+              type='radio'
+              style={{ display: 'none' }}
+              value='partner'
+              {...register('tag', { required: true })}
+            />
+            <input
               id='work'
               type='radio'
               style={{ display: 'none' }}
@@ -94,10 +215,17 @@ export function AddressForm() {
               {...register('tag', { required: true })}
             />
             <input
-              id='partner'
+              id='school'
               type='radio'
               style={{ display: 'none' }}
-              value='partner'
+              value='school'
+              {...register('tag', { required: true })}
+            />
+            <input
+              id='other'
+              type='radio'
+              style={{ display: 'none' }}
+              value='other'
               {...register('tag', { required: true })}
             />
 
@@ -110,7 +238,20 @@ export function AddressForm() {
             <label htmlFor='work' style={watch('tag') === 'work' ? selectedTagLabelStyle : tagLabelStyle}>
               <PiSuitcaseSimple size={'18px'} /> Trabalho
             </label>
+            <label htmlFor='school' style={watch('tag') === 'school' ? selectedTagLabelStyle : tagLabelStyle}>
+              <IoSchoolOutline size={'18px'} /> Faculdade
+            </label>
+            <label htmlFor='other' style={watch('tag') === 'other' ? selectedTagLabelStyle : tagLabelStyle}>
+              Outro
+            </label>
           </div>
+        </div>
+        <div style={{
+          marginTop: designTokens.spacing.medium,
+          display: 'flex',
+          width: '100%',
+        }}>
+          <Button label='Adicionar' type='primary' onClick={() => { }} />
         </div>
       </form>
     </div>
--- src/pages/Profile/components/AddressSection/AdressSection.tsx
+++ src/pages/Profile/components/AddressSection/AdressSection.tsx
@@ -6,16 +6,107 @@ export function AddressSection() {
 
   const adresses = [
     {
+      cep: '12345-678',
       street: 'Rua das Laranjeiras',
       number: 123,
-      district: 'Centro'
+      district: 'Centro',
+      city: 'São Paulo',
     },
     {
+      cep: '98765-432',
       street: 'Rua das Flores',
       number: 456,
-      district: 'Jardim'
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
+    },
+    {
+      cep: '98765-432',
+      street: 'Rua das Flores',
+      number: 456,
+      district: 'Jardim',
+      city: 'Rio de Janeiro',
     }
   ]
+
+  const heightForm = document.getElementById('address-form')?.clientHeight
+    || document.getElementById('address-form')?.offsetHeight
 
   return (
     <div style={{
@@ -24,16 +115,16 @@ export function AddressSection() {
       width: '100%',
       height: '100%',
     }}>
-      <div style={{
+      <div id='address-form' style={{
         width: '100%',
-        height: `calc(50% - ${designTokens.spacing.large})`,
         marginBottom: designTokens.spacing.large
       }}>
         <AddressForm />
       </div>
       <div style={{
         width: '100%',
-        height: '50%',
+        flex: 1,
+        maxHeight: `calc(100% - ${designTokens.spacing.large} - ${heightForm}px)`,
       }}>
         <AddressList addresses={adresses} />
       </div>
