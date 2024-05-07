e68c711ffd5b9b06710c61441e31dd2c6dfacd6f
TheusBr05 <mat.587263@gmail.com>
2024-05-07 13:16:09 -0300

    Atualizar dados de exemplo do usuário

--- src/pages/Profile/components/FormProfile/FormProfile.tsx
+++ src/pages/Profile/components/FormProfile/FormProfile.tsx
@@ -1,5 +1,6 @@
 import { designTokens } from 'design-tokens'
 import { RiAccountCircleLine } from 'react-icons/ri'
+import monteiro from '../../../../assets/monteiro.png'
 
 export const FormProfile = () => {
   const textInputStyle = {
@@ -34,7 +35,8 @@ export const FormProfile = () => {
       height: `calc(100% - ${designTokens.spacing.large} * 2)`,
       marginRight: designTokens.spacing.large,
       padding: designTokens.spacing.large,
-      borderRadius: designTokens.borderRadius.large
+      borderRadius: designTokens.borderRadius.large,
+      gap: designTokens.spacing.medium,
     }}>
       <span style={{
         fontFamily: designTokens.font.family,
@@ -49,7 +51,8 @@ export const FormProfile = () => {
           display: 'flex',
           flexDirection: 'column',
           padding: '0px 24px',
-          gap: 22
+          gap: 22,
+          flex: 1
         }}
       >
         <section>
@@ -58,41 +61,45 @@ export const FormProfile = () => {
             height: 100,
             backgroundSize: 'cover',
             borderRadius: 100,
-            backgroundImage: 'url(https://pbs.twimg.com/profile_images/1681409436441620480/TLxv4-aY_400x400.jpg)'
+            backgroundImage: `url(${monteiro})`
           }}></div>
         </section>
 
         <section style={{
           display: 'flex',
+          flex: 1,
           flexDirection: 'column',
-          gap: 16,
         }}>
-          <div style={groupLabelInputStyle as React.CSSProperties}>
-            <label style={{ flexDirection: 'column' }}>Nome</label>
-            <input placeholder='Manoel Gomes' style={textInputStyle} type='text' />
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+          }}>
+            <div style={groupLabelInputStyle as React.CSSProperties}>
+              <label style={{ flexDirection: 'column' }}>Nome</label>
+              <input placeholder='Monteiro' style={textInputStyle} type='text' />
+            </div>
+
+            <div style={groupLabelInputStyle as React.CSSProperties}>
+              <label>Endereço</label>
+              <input placeholder='Rua Haddock Lobo' style={textInputStyle} type='text' />
+            </div>
+
+            <div style={groupLabelInputStyle as React.CSSProperties}>
+              <label>Gênero</label>
+              <select style={selectInputStyle}>
+                <option style={{ fontSize: 16 }} value='M'>Masculino</option>
+                <option style={{ fontSize: 16 }} value='F'>Feminino</option>
+                <option style={{ fontSize: 16 }} value='O'>Outros</option>
+                <option style={{ fontSize: 16 }} value='O'>Prefiro não dizer</option>
+              </select>
+            </div>
+
+            <div style={groupLabelInputStyle as React.CSSProperties}>
+              <label>Data de Nascimento</label>
+              <input placeholder='1969-11-02' style={textInputStyle} type='date' value='1969-11-02' />
+            </div>
           </div>
-
-          <div style={groupLabelInputStyle as React.CSSProperties}>
-            <label>Endereço</label>
-            <input placeholder='Rua Manoel Gomes' style={textInputStyle} type='text' />
-          </div>
-
-          <div style={groupLabelInputStyle as React.CSSProperties}>
-            <label>Gênero</label>
-            <select style={selectInputStyle}>
-              <option style={{ fontSize: 16 }} value='F'>Feminino</option>
-              <option style={{ fontSize: 16 }} value='M'>Masculino</option>
-              <option style={{ fontSize: 16 }} value='O'>Outros</option>
-              <option style={{ fontSize: 16 }} value='O'>Prefiro não dizer</option>
-            </select>
-          </div>
-
-          <div style={groupLabelInputStyle as React.CSSProperties}>
-            <label>Data de Nascimento</label>
-            <input placeholder='02/11/1969' style={textInputStyle} type='date' />
-          </div>
-
-
           <button style={{
             width: 140,
             height: 50,
