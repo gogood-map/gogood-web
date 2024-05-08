dd3f161f136ba6d6bda380779aa28ea6d7ec70b0
TheusBr05 <mat.587263@gmail.com>
2024-05-08 00:22:13 -0300

    Adicionar função de fechamento card e função de retorno de risco

--- src/pages/Map/components/RoutesSelection/RoutesSelection.tsx
+++ src/pages/Map/components/RoutesSelection/RoutesSelection.tsx
@@ -1,34 +1,62 @@
-import { designTokens } from "design-tokens";
-import { useContext } from "react";
-import { RouteSearchCardContext, RouteSearchCardContextProps } from "../RouteSearchCard/RouteSearchCard";
-import { RouteOption } from "../RouteOption/RouteOption";
-import { LoaderIcon } from "../../../../components/LoaderIcon/LoaderIcon";
+import { designTokens } from 'design-tokens'
+import { useContext } from 'react'
+import { RouteSearchCardContext, RouteSearchCardContextProps } from '../RouteSearchCard/RouteSearchCard'
+import { RouteOption } from '../RouteOption/RouteOption'
+import { LoaderIcon } from '../../../../components/LoaderIcon/LoaderIcon'
+import { IoClose } from 'react-icons/io5'
 
 export function routesColors(routes: RoutesResponse[]) {
     let colors = routes.map(route => {
-        const ocorrenciasPorKm = route.qtdOcorrenciasTotais / route.distancia;
+        const ocorrenciasPorKm = route.qtdOcorrenciasTotais / route.distancia
         if (ocorrenciasPorKm < 75) {
-            return designTokens.color.success;
+            return designTokens.color.success
         } else if (ocorrenciasPorKm < 150) {
-            return designTokens.color.alert;
+            return designTokens.color.alert
         } else {
-            return designTokens.color.error;
+            return designTokens.color.error
         }
     })
 
     if (colors.every(color => color === designTokens.color.error)) {
-        colors[0] = designTokens.color.success;
-        colors[1] = designTokens.color.alert;
+        colors[0] = designTokens.color.success
+        colors[1] = designTokens.color.alert
     } else if (colors.every(color => color === designTokens.color.alert)) {
-        colors[0] = designTokens.color.success;
+        colors[0] = designTokens.color.success
     } else if (colors.every(color => color === designTokens.color.success)) {
-        colors = colors;
+        colors = colors
     } else if (!colors.includes(designTokens.color.success)) {
-        colors[0] = designTokens.color.success;
-        colors[1] = designTokens.color.alert;
+        colors[0] = designTokens.color.success
+        colors[1] = designTokens.color.alert
     }
 
-    return colors;
+    return colors
+}
+
+export function routesRisk(routes: RoutesResponse[]) {
+    let risk = routes.map(route => {
+        const ocorrenciasPorKm = route.qtdOcorrenciasTotais / route.distancia
+        if (ocorrenciasPorKm < 75) {
+            return 'Baixo Risco'
+        } else if (ocorrenciasPorKm < 150) {
+            return 'Médio Risco'
+        } else {
+            return 'Alto Risco'
+        }
+    })
+
+    if (risk.every(risk => risk === 'Alto Risco')) {
+        risk[0] = 'Baixo Risco'
+        risk[1] = 'Médio Risco'
+    } else if (risk.every(risk => risk === 'Médio Risco')) {
+        risk[0] = 'Baixo Risco'
+    } else if (risk.every(risk => risk === 'Baixo Risco')) {
+        risk = risk
+    } else if (!risk.includes('Baixo Risco')) {
+        risk[0] = 'Baixo Risco'
+        risk[1] = 'Médio Risco'
+    }
+
+    return risk
 }
 
 export type RoutesResponse = {
@@ -49,47 +77,48 @@ type RoutesSelectionProps = {
     routes?: RoutesResponse[]
     searchStatus: 'loading' | 'success' | 'error' | 'none'
     onSelectRoute?: (route: RoutesResponse) => void
+    onClose?: () => void
 }
 
 export function RoutesSelection(props: RoutesSelectionProps) {
-    const { routes, searchStatus, onSelectRoute } = props
+    const { routes, searchStatus, onSelectRoute, onClose } = props
     const { expandedCard } = useContext(RouteSearchCardContext) as RouteSearchCardContextProps
 
     const orderedRoutes = (routes: RoutesResponse[]) => routes.sort((a, b) => {
-        const durationA = a.qtdOcorrenciasTotais;
-        const durationB = b.qtdOcorrenciasTotais;
+        const durationA = a.qtdOcorrenciasTotais
+        const durationB = b.qtdOcorrenciasTotais
 
-        return durationA - durationB;
-    });
+        return durationA - durationB
+    })
 
     const stringToMinutes = (duration: string) => {
-        const parts = duration.split(' ');
-        let totalMinutos = 0;
+        const parts = duration.split(' ')
+        let totalMinutos = 0
 
         for (let i = 0; i < parts.length; i += 2) {
-            const value = parseInt(parts[i], 10);
-            const unit = parts[i + 1];
+            const value = parseInt(parts[i], 10)
+            const unit = parts[i + 1]
 
             switch (unit) {
                 case 'hora':
                 case 'horas':
-                    totalMinutos += value * 60;
-                    break;
+                    totalMinutos += value * 60
+                    break
                 case 'minuto':
                 case 'minutos':
-                    totalMinutos += value;
-                    break;
+                    totalMinutos += value
+                    break
                 default:
-                    throw new Error(`Unidade inválida: ${unit}`);
+                    throw new Error(`Unidade inválida: ${unit}`)
             }
         }
 
-        return totalMinutos;
+        return totalMinutos
     }
 
     const sendSelectedRoute = (route: RoutesResponse) => {
-        onSelectRoute && onSelectRoute(route);
-        console.table(route);
+        onSelectRoute && onSelectRoute(route)
+        console.table(route)
     }
 
     const height = routes && expandedCard && searchStatus === 'success'
@@ -115,7 +144,17 @@ export function RoutesSelection(props: RoutesSelectionProps) {
             borderRadius: designTokens.borderRadius.medium,
             boxShadow: `0 4px 14px 0 ${designTokens.color.boxShadow}`,
             transition: 'height 0.3s ease',
+            position: 'relative'
         }}>
+            <div onClick={onClose} style={{
+                display: 'flex',
+                position: 'absolute',
+                top: designTokens.spacing.tiny,
+                right: designTokens.spacing.tiny,
+                cursor: 'pointer',
+            }}>
+                <IoClose size={20} color={designTokens.color.text} />
+            </div>
             <div style={{
                 display: 'flex',
                 justifyContent: 'center',
@@ -143,10 +182,16 @@ export function RoutesSelection(props: RoutesSelectionProps) {
             }}>
 
                 {routes && searchStatus === 'success' && orderedRoutes(routes).map((route, index) => {
-                    const durationInMinutes = stringToMinutes(route.duracao);
-                    const color = routesColors(routes)[index];
-                    const risk = index === 0 ? 'Menor Risco' : index === 1 ? 'Risco Médio' : 'Risco Alto';
-                    return <RouteOption key={index} risk={risk} durationInMinutes={durationInMinutes} color={color} onClick={() => sendSelectedRoute(route)} />;
+                    const durationInMinutes = stringToMinutes(route.duracao)
+                    const color = routesColors(routes)[index]
+                    const risk = routesRisk(routes)[index]
+                    return <RouteOption
+                        key={index}
+                        risk={risk}
+                        durationInMinutes={durationInMinutes}
+                        color={color}
+                        onClick={() => sendSelectedRoute(route)}
+                    />
                 })}
 
                 {searchStatus === 'loading' && <div style={{
