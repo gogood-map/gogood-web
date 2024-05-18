88a491d11911ef671f520cc8a41b0408c4afa47a
TheusBr05 <mat.587263@gmail.com>
2024-05-18 03:42:57 -0300

    Adicionar react-toastify para exibir notificações ao usuário

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import { routes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './hooks/AuthProvider/AuthProvider'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-    const clientId = "112629213943-ltci50f3petj9rfckhofatc6sve64btj.apps.googleusercontent.com"
+    const clientId = '112629213943-ltci50f3petj9rfckhofatc6sve64btj.apps.googleusercontent.com'
 
     return (
-        <BrowserRouter>
-            <GoogleOAuthProvider clientId={clientId ? clientId : ''}>
-                <AuthProvider>
-                    {routes}
-                </AuthProvider>
-            </GoogleOAuthProvider>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <GoogleOAuthProvider clientId={clientId ? clientId : ''}>
+                    <AuthProvider>
+                        {routes}
+                    </AuthProvider>
+                </GoogleOAuthProvider>
+            </BrowserRouter>
+            <ToastContainer />
+        </>
     )
 }
 
