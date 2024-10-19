d1f1b6725ca859880a87861348a4412966c0f3fc
TheusBr05 <mat.587263@gmail.com>
2024-10-19 14:26:04 -0300

    Remove stories de componentes

--- src/pages/History/components/HistoryTableItem/HistoryTableItem.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { HistoryTableItem } from './HistoryTableItem'
-
-export default {
-  title: 'Components/HistoryTableItem',
-  component: HistoryTableItem,
-}
-
-export const Default = () => <HistoryTableItem date="Quinta-feira, 25 Jan, 2024" origin="Rua A, 123" destination="Rua B, 456" onClick={() => console.log('clicked')} />
--- src/pages/History/components/HitoryNav/HistoryNav.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { HistoryNav } from './HistoryNav'
-
-export default {
-  title: 'Components/HistoryNav',
-  component: HistoryNav,
-}
-
-export const Default = () => <HistoryNav />
