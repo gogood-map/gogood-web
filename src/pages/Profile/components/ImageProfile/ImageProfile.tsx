fe7bd3e06ac9a72becc63df898fc438a22b4dbf7
TheusBr05 <mat.587263@gmail.com>
2024-11-06 22:45:29 -0300

    Refatora componente ImageProfile para utilizar avatar gerado e buscar imagem do usuário

--- src/pages/Profile/components/ImageProfile/ImageProfile.tsx
+++ src/pages/Profile/components/ImageProfile/ImageProfile.tsx
@@ -1,24 +1,52 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
+import { createAvatar } from '@dicebear/core'
+import { initials } from '@dicebear/collection'
+import { useAuth } from '../../../../hooks/AuthProvider/AuthProvider'
+import { getUserImage } from '../../../../utils/requests/user'
 
+export function ImageProfile() {
+  const [image, setImage] = useState<string | null>()
+  const { user } = useAuth()
 
-export type ImageProfileProps = {
-  userImage?: Blob
-}
-
-export function ImageProfile(props: ImageProfileProps) {
-  const { userImage } = props
-  const [image, setImage] = useState<string | null>(null)
+  const createAvatarImage = () => {
+    if (user) {
+      const svg = createAvatar(initials, {
+        seed: user.name
+      })
+      setImage(svg.toDataUri())
+    }
+  }
 
   useEffect(() => {
-    if (!userImage) {
-      axios.get('https://api.github.com/users/rafaelbogfreitas')
+    if (user) {
+      getUserImage(user.id).then((response) => {
+        console.log('Imagem', response.data)
+        if (response.data.size === 0) {
+          createAvatarImage()
+          return
+        } else {
+          setImage(URL.createObjectURL(response.data))
+        }
+      }).catch((error) => {
+        console.log('Erro ao buscar imagem do usuário')
+        console.log(error)
+        createAvatarImage()
+      })
     }
   }, [])
 
   return (
-    <section>
-
+    <section style={{
+      position: 'relative',
+    }}>
+      <img style={{
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        objectFit: 'cover',
+      }}
+        src={image || ''}
+      />
     </section>
   )
 }
