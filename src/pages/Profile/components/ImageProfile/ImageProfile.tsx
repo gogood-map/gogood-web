d6eac5a4325db741e9e16b0ca7483b1053abc659
TheusBr05 <mat.587263@gmail.com>
2024-12-01 22:36:31 -0300

    Adiciona estilos e tooltip para o componente de imagem de perfil

--- src/pages/Profile/components/ImageProfile/ImageProfile.tsx
+++ src/pages/Profile/components/ImageProfile/ImageProfile.tsx
@@ -1,157 +1,123 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { createAvatar } from '@dicebear/core';
-import { initials } from '@dicebear/collection';
-import { useAuth } from '../../../../hooks/AuthProvider/AuthProvider';
-import { getUserImage, updateUserImage, deleteUserImage } from '../../../../utils/requests/user';
-import { LoaderIcon } from '../../../../components/LoaderIcon/LoaderIcon';
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './ImageProfile.module.css'
+import { createAvatar } from '@dicebear/core'
+import { initials } from '@dicebear/collection'
+import { useAuth } from '../../../../hooks/AuthProvider/AuthProvider'
+import { getUserImage, updateUserImage, deleteUserImage } from '../../../../utils/requests/user'
+import { LoaderIcon } from '../../../../components/LoaderIcon/LoaderIcon'
+import { PiCamera, PiTrash } from 'react-icons/pi'
 
 export function ImageProfile() {
-  const [image, setImage] = useState<string | null>(null);
-  const [tooltipVisible, setTooltipVisible] = useState(false);
-  const [updateStatus, setUpdateStatus] = useState(false); // Indica o carregamento
-  const { user } = useAuth();
-  const tooltipRef = useRef<HTMLDivElement | null>(null); // Referência ao tooltip
+  const [image, setImage] = useState<string | null>(null)
+  const [tooltipVisible, setTooltipVisible] = useState(false)
+  const [updateStatus, setUpdateStatus] = useState(false)
+  const { user } = useAuth()
+  const tooltipRef = useRef<HTMLDivElement | null>(null)
 
   const createAvatarImage = () => {
     if (user) {
       const svg = createAvatar(initials, {
         seed: user.name,
         scale: 75,
-      });
-      setImage(svg.toDataUri());
+      })
+      setImage(svg.toDataUri())
     }
-  };
+  }
 
   const getUserImageProfile = () => {
     if (user) {
       getUserImage(user.id)
         .then((response) => {
           if (response.data.size === 0) {
-            createAvatarImage();
-            return;
+            createAvatarImage()
+            return
           } else {
-            setImage(URL.createObjectURL(response.data));
+            setImage(URL.createObjectURL(response.data))
           }
         })
         .catch((error) => {
-          console.error('Erro ao buscar imagem do usuário', error);
-          createAvatarImage();
-        });
+          console.error('Erro ao buscar imagem do usuário', error)
+          createAvatarImage()
+        })
     }
-  };
+  }
 
   const updateImageProfile = (file: Blob) => {
     if (file && user) {
-      setUpdateStatus(true);
+      setUpdateStatus(true)
       updateUserImage(user.id, file)
         .then(() => {
-          setUpdateStatus(false);
+          setUpdateStatus(false)
         })
         .catch((error) => {
-          console.error('Erro ao atualizar imagem do usuário', error);
-          setUpdateStatus(false);
-        });
+          console.error('Erro ao atualizar imagem do usuário', error)
+          setUpdateStatus(false)
+        })
     }
-  };
+  }
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const file = event.target.files?.[0]
     if (file) {
-      setImage(URL.createObjectURL(file)); // Atualiza visualmente a imagem
-      updateImageProfile(file); // Envia a imagem para o backend
+      setImage(URL.createObjectURL(file))
+      updateImageProfile(file)
     }
-  };
+  }
 
   const handleDeleteImage = () => {
     if (user) {
       deleteUserImage(user.id)
         .then(() => {
-          createAvatarImage();
+          createAvatarImage()
         })
         .catch((error) => {
-          console.error('Erro ao deletar imagem do usuário', error);
-        });
+          console.error('Erro ao deletar imagem do usuário', error)
+        })
     }
   }
 
-  const handleToggleTooltip = () => setTooltipVisible((prev) => !prev);
+  const handleToggleTooltip = () => setTooltipVisible((prev) => !prev)
 
   const handleClickOutside = (event: MouseEvent) => {
     if (
       tooltipRef.current &&
       !tooltipRef.current.contains(event.target as Node)
     ) {
-      setTooltipVisible(false);
+      setTooltipVisible(false)
     }
-  };
+  }
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside); // Adiciona o listener global
+    document.addEventListener('mousedown', handleClickOutside)
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside); // Remove o listener no cleanup
-    };
-  }, []);
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
   useEffect(() => {
-    getUserImageProfile();
-  }, [user]);
+    getUserImageProfile()
+  }, [user])
 
   return (
     <section>
-      <span
-        style={{
-          position: 'relative',
-          display: 'inline-block',
-        }}
-      >
-        {/* Imagem do perfil */}
+      <span className={styles['div-main']}>
         <img
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: '50%',
-            objectFit: 'cover',
-            cursor: 'pointer', // Corrigido o cursor
-          }}
+          className={styles['image-profile']}
           src={image || ''}
           onClick={handleToggleTooltip}
           alt="Imagem do Perfil"
         />
 
-        {/* Tooltip */}
         {tooltipVisible && (
           <div
-            ref={tooltipRef} // Referência ao tooltip para capturar cliques fora
-            style={{
-              position: 'absolute',
-              top: '110%',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              background: '#fff',
-              border: '1px solid #ccc',
-              borderRadius: '8px',
-              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-              zIndex: 10,
-              padding: '10px',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '8px',
-            }}
+            ref={tooltipRef}
+            className={styles['tooltip']}
           >
             <label
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '8px',
-                cursor: 'pointer',
-                padding: '8px 12px',
-                borderRadius: '4px',
-                fontSize: '14px',
-                transition: 'background 0.2s',
-                background: '#f3f3f3',
-              }}
+             className={styles['label-tooltip']}
             >
-              📷 Carregar foto
+              <PiCamera size={20} />
+              Carregar foto
               <input
                 type="file"
                 accept="image/*"
@@ -160,23 +126,14 @@ export function ImageProfile() {
               />
             </label>
             <button
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '8px',
-                background: 'none',
-                border: 'none',
-                cursor: 'pointer',
-                padding: '8px 12px',
-                borderRadius: '4px',
-                fontSize: '14px',
-              }}
+              className={styles['label-tooltip']}
               onClick={(event) => {
-                event.preventDefault(); // Previne o recarregamento
-                handleDeleteImage();
+                event.preventDefault()
+                handleDeleteImage()
               }}
             >
-              🗑️ Apagar foto
+              <PiTrash size={20} />
+              Apagar foto
             </button>
           </div>
         )}
@@ -185,10 +142,12 @@ export function ImageProfile() {
           <div
             style={{
               position: 'absolute',
+              fontSize: '32px',
               top: '50%',
               left: '50%',
               transform: 'translate(-50%, -50%)',
-              background: 'rgba(255, 255, 255, 0.8)',
+              background: 'transparent',
+              color: '#12FFBB',
               borderRadius: '50%',
             }}
           >
@@ -197,5 +156,5 @@ export function ImageProfile() {
         )}
       </span>
     </section>
-  );
+  )
 }
